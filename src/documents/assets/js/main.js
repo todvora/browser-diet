87ed7f452d3fb470a0b724e943bd0138e01645a0
Keppelen <keppelen.as@gmail.com>
2013-05-14 15:56:20 -0300

    update navbar

--- src/documents/assets/js/main.js
+++ src/documents/assets/js/main.js
@@ -1,26 +1,59 @@
-$('.animate').bind('click',function(e){
-    var id = $(this).attr('href');
+// $('.animate').bind('click',function(e){
+//     var id = $(this).attr('href');
 
-    e.preventDefault();
-    $('html,body').animate({scrollTop: $(''+id+'').offset().top},'slow');
-});
+//     e.preventDefault();
+//     $('html,body').animate({scrollTop: $(''+id+'').offset().top},'slow');
+// });
 
-$("#menu a").on("click", function(){
-  var location = $(this).attr('href').replace("#","");
-  $(".topics").css("padding-top", "0");
-  $("#" + location).css("padding-top", "95px");
-})
+// $("#menu a").on("click", function(){
+//   var location = $(this).attr('href').replace("#","");
+//   $(".topics").css("padding-top", "0");
+//   $("#" + location).css("padding-top", "95px");
+// })
 
-$(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 560) {
-      $("#menu").addClass("fixing-menu");
-      $("#github-ribbon").hide();
-      $("#top").show();
-    } else {
-      $("#menu").removeClass("fixing-menu");
-      $("#github-ribbon").show();
-      $("#top").hide();
-    }
-  });
-});
+// $(function () {
+//   $(window).scroll(function () {
+//     if ( this.scrollTop() > 560) {
+//       // $("#menu").addClass("fixing-menu");
+//       // $("#github-ribbon").hide();
+//       // $("#top").show();
+//       console.log("if");
+//     } else {
+//       // $("#menu").removeClass("fixing-menu");
+//       // $("#github-ribbon").show();
+//       // $("#top").hide();
+//     }
+//   });
+// });
+// getElementsByClassName
+
+// function scrola($sobe_ou_desce){
+//     if($sobe_ou_desce=='sobe'){
+//         while(document.body.scrollTop>1){
+//             window.scrollTo(0,document.body.scrollTop - 3);
+//         }
+//     }else{
+//         while(document.body.scrollTop<document.body.scrollHeight - document.body.clientHeight){
+//             window.scrollTo(0,document.body.scrollTop + 3);
+//         }
+//     }
+// }
+// scrola();
+// console.log( scrola("sobe") );
+
+
+
+
+
+// while( document.body.scroll ) {
+//   console.log( document.body.scroll );
+// }
+
+  console.log( document.body.scroll(0,560) );
+
+
+
+
+
+
+
